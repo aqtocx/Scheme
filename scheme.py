63916681beabedd3358bdfa008fe3b9d1e335ecf
Jiashu Han <jiashu.han@berkeley.edu>
2016-11-04 21:20:53 -0700

    Q6 done

--- scheme.py
+++ scheme.py
@@ -207,7 +207,8 @@ def do_define_form(expressions, env):
     if scheme_symbolp(target):
         check_form(expressions, 2, 2)
         # BEGIN PROBLEM 6
-        "*** REPLACE THIS LINE ***"
+        env.define(target, scheme_eval(expressions.second.first, env))
+        return target
         # END PROBLEM 6
     elif isinstance(target, Pair) and scheme_symbolp(target.first):
         # BEGIN PROBLEM 10
